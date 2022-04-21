778d8397e93e944d26eeb64aff505827628c63d0
Emelie Rosendahl <89969808+Erm0es@users.noreply.github.com>
2022-04-22 00:17:07 +0200

    Layout component and styling

---
+++ components/Navbar.js
@@ -0,0 +1,19 @@
+import Link from 'next/link'
+
+const Navbar = () => {
+  return (
+    <nav>
+        <div className="logo">
+            <h1>Kino On Mars</h1>
+        </div>
+        <div className="links">
+        <Link href="/"><a>Home</a></Link>
+        <a>Movies</a>
+        <a>Tickets</a>
+        <a>About</a>
+        </div>
+    </nav>
+  );
+}
+
+export default Navbar
