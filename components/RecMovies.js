ec73db071bab3879a08787fa742730f30af456cc
Emelie Rosendahl <89969808+Erm0es@users.noreply.github.com>
2022-04-28 11:02:17 +0200

    Borders for rec movies

--- components/RecMovies.js
+++ components/RecMovies.js
@@ -1,9 +1,23 @@
-import React from 'react'
+import React from 'react';
+import styles from '../styles/RecMovies.module.css';
 
 function RecMovies() {
   return (
     <div>
+    <div className={styles.heading}>
         <h1>Kino Recommends</h1>
+    </div>
+    <div className={styles.rec}>
+        <div className={styles.movie}>
+            <h1>MOVIE 1</h1>
+        </div>
+        <div className={styles.movie}>
+            <h1>MOVIE 2</h1>
+        </div>
+        <div className={styles.movie}>
+            <h1>MOVIE 3</h1>
+        </div>
+    </div>
     </div>
   )
 }
