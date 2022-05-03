468c1dc93e1a95e9fe6ceae99e18ecbc60b8ee76
Anders Myrman <amyhrman@gmail.com>
2022-05-03 15:32:52 +0200

    Add mapping function with db retrieval

--- pages/movies.js
+++ pages/movies.js
@@ -1,7 +1,32 @@
-function Movies() {
-  return <div>
-    Movies
-  </div>
+import dbConnect from '../lib/dbConnect';
+import Movie from '../models/movie';
+
+import styles from './movies.module.css';
+
+const MoviesPage = ({ movies }) => (
+  <>
+    {movies.map((movie) => (
+      <div key={movie._id}>
+        <div>
+          <img src={movie.imgUrl} />
+        </div>
+      </div>
+    ))}
+  </>
+)
+
+/* Retrieve movies data from mongodb database */
+export async function getServerSideProps() {
+  await dbConnect();
+
+  const result = await Movie.find({})
+  const movies = result.map((doc) => {
+    const movie = doc.toObject()
+    movie._id = movie._id.toString()
+    return movie
+  })
+
+  return { props: { movies: movies } }
 }
 
-export default Movies
+export default MoviesPage
