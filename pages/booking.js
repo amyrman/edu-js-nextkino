fbd4ccd3402873fec8fd07f6367d2d9e663fce87
Emil Noren <stratosfloor@hotmail.com>
2022-05-03 06:33:48 +0200

    Added states for differnt parts of booking

--- pages/booking.js
+++ pages/booking.js
@@ -1,16 +1,26 @@
 import { useState, useEffect } from "react";
-import { useRouter } from "next/router";
 
 import BookingForm from "../components/BookingForm";
 import NumOfTickets from "../components/NumOfTickets";
 import PaymentModule from "../components/PaymentModule";
 import Seating from "../components/Seating";
 import StickyBooking from "../components/StickyBooking";
-import styles from "../styles/booking.module.css";
+import styles from "../styles/Booking.module.css";
+import BackBtn from "../components/BackBtn";
 
 export default function Booking() {
+  // Bookingpage has to have a screeningId to start
+  //
+  const [bookingState, setBookingState] = useState(false);
   const [numTickets, setNumTickets] = useState(2);
-  const router = useRouter();
+
+  const handleNumTickets = (newnum) => {
+    setNumTickets(newnum);
+  };
+
+  const handleBookingState = (state) => {
+    setBookingState(state);
+  };
 
   // Mockinfo
   const bookingInfo = {
@@ -22,26 +32,48 @@ export default function Booking() {
     price: numTickets * 15,
   };
 
-  const handleNumTickets = (newnum) => {
-    setNumTickets(newnum);
-  };
-
-  return (
-    <div className={styles.container}>
-      <h1>Booking!</h1>
-      <div>
-        <button className={styles.backBtn} onClick={() => router.back()}>
-          <small> &#10094;back</small>
-        </button>
-        <NumOfTickets
-          handleNumTickets={handleNumTickets}
-          numTickets={numTickets}
-        />
-        <Seating />
-        <BookingForm bookingInfo={bookingInfo} />
-        <PaymentModule />
+  // Change to check for valid screening ID
+  //
+  if (bookingState == true) {
+    return (
+      <div className={styles.container}>
+        <h1>Booking!</h1>
+        <div>
+          <BackBtn />
+          <NumOfTickets
+            handleNumTickets={handleNumTickets}
+            numTickets={numTickets}
+          />
+          <Seating />
+          <BookingForm
+            bookingInfo={bookingInfo}
+            setBookingState={handleBookingState}
+          />
+          <PaymentModule />
+        </div>
+        <StickyBooking bookingInfo={bookingInfo} numTickets={numTickets} />
       </div>
-      <StickyBooking bookingInfo={bookingInfo} numTickets={numTickets} />
-    </div>
-  );
+    );
+    // Completed booking
+    //
+  } else if (bookingState == "completed") {
+    return (
+      <div className={styles.container}>
+        <h2> Booking completed</h2>
+      </div>
+    );
+  } else {
+    // If trying to reach bookingpage without valid screening ID
+    //
+    return (
+      <div className={styles.container}>
+        <h2>No screenning info found</h2>
+        <BackBtn />
+        {/* Temporery */}
+        <button onClick={() => setBookingState(true)}>
+          <p>Bokning med screening ID</p>
+        </button>
+      </div>
+    );
+  }
 }
