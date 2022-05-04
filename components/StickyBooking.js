37f47102837b02f57ffddca950e4875f15b48dab
Emil Noren <stratosfloor@hotmail.com>
2022-05-04 12:53:59 +0200

    Refactor for real screening object

--- components/StickyBooking.js
+++ components/StickyBooking.js
@@ -1,15 +1,13 @@
 import styles from "./StickyBooking.module.css";
-const StickyBooking = ({ bookingInfo }) => {
+const StickyBooking = ({ screening, numTickets }) => {
   return (
     <div className={styles.container}>
       <p className={styles.header}>Booking info</p>
-      <p className={styles.tickets}>
-        Numer of tickets: {bookingInfo.numTickets}
-      </p>
-      <p className={styles.movie}>{bookingInfo.movie}</p>
-      <p className={styles.date}>{bookingInfo.date}</p>
-      <p className={styles.time}> Time: {bookingInfo.time} </p>
-      <p className={styles.money}>{bookingInfo.price}€£$</p>
+      <p className={styles.tickets}>Numer of tickets: {numTickets}</p>
+      <p className={styles.movie}>{screening.title}</p>
+      <p className={styles.date}>{screening.date}</p>
+      <p className={styles.time}> Time: {screening.time} </p>
+      <p className={styles.money}>{numTickets * 15}€£$</p>
     </div>
   );
 };
