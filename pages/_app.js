1b59036ebac8a6d5f5f9b98b9a6ad5ced853b6de
Anders Myrman <amyhrman@gmail.com>
2022-05-09 20:58:32 +0200

    Delete unused imports

--- pages/_app.js
+++ pages/_app.js
@@ -3,10 +3,7 @@ import '../styles/globals.css'
 import * as React from 'react';
 import PropTypes from 'prop-types';
 import Head from 'next/head';
-import { ThemeProvider } from '@mui/material/styles';
-import CssBaseline from '@mui/material/CssBaseline';
 import { CacheProvider } from '@emotion/react';
-import theme from '../src/theme';
 import createEmotionCache from '../src/createEmotionCache';
 
 // Client-side cache, shared for the whole session of the user in the browser.
