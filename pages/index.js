b25a0c806ace32794a809b9c810577b1aa0c3f9b
Emelie Wiberg <89969808+Erm0es@users.noreply.github.com>
2022-05-11 13:03:03 +0200

    fixed

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,3 @@
-import Head from 'next/head'
-import Footer from '../components/Footer'
-import Navbar from '../components/Navbar'
 import styles from '../styles/Home.module.css'  
 import Herosection from './herosection'
 import RecMovies from '../components/RecMovies'
